b229a7a3f1ab3026de1be18164e97de36bb59519
Luis <luisrs8608@gmail.com>
2019-05-03 02:58:44 -0300

    adding department field to invoices

--- account_extend/models/account_invoice.py
+++ account_extend/models/account_invoice.py
@@ -11,6 +11,7 @@ class AccountInvoice(models.Model):
 
     # compute and search fields, in the same order that fields declaration
     @api.multi
+    @api.depends('user_id')
     def _compute_department_id(self):
         for rec in self:
             employee_id = self.env['hr.employee'].search([('user_id', '=', self.env.user.id)], limit=1)
