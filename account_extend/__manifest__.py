4d564e4aa4964ee9345e9c0e1744e02c1230ad6b
Luis <luisrs8608@gmail.com>
2019-05-03 02:43:22 -0300

    Starting project, extend account addon

---
+++ account_extend/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Account extend',
+    'version': '12.0.1.0.0',
+    'summary': 'Account extend',
+    'sequence': 16,
+    'description': """
+    """,
+    'category': 'Invoicing Management',
+    'website': '',
+    'images': [],
+    'depends': ['account'],
+    'data': [
+        'views/account_invoice_view.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'qweb': [],
+}
